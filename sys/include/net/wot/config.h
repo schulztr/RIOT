57ce0924cef1d59ca98afb6de84c11459d30d976
Philipp Blum <philipp-blum@jakiku.de>
2020-09-20 01:08:27 +0200

    Some updates for WoT

---
+++ sys/include/net/wot/config.h
@@ -0,0 +1,8 @@
+//
+// Created by citrullin on 19.09.20.
+//
+
+#ifndef RIOT_CONFIG_H
+#define RIOT_CONFIG_H
+
+#endif //RIOT_CONFIG_H
---
+++ tests/wot_coap/main.c
@@ -0,0 +1,4 @@
+//
+// Created by citrullin on 18.09.20.
+//
+
