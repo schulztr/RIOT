101132ed70a6c0d0b827274d9bdaae33fbe95c5f
Philipp-Alexander Blum <philipp-blum@jakiku.de>
2020-12-21 17:11:11 +0100

    Fix data_schema = NULL error

--- tests/wot_coap_complex/wot_coap_config.c
+++ tests/wot_coap_complex/wot_coap_config.c
@@ -9,11 +9,11 @@ static ssize_t _echo_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *co
 
     if (coap_get_uri_path(pkt, (uint8_t *)uri) <= 0) {
         return coap_reply_simple(pkt, COAP_CODE_INTERNAL_SERVER_ERROR, buf,
-                                 len, COAP_FORMAT_TEXT, NULL, 0);
+                                 len, COAP_FORMAT_JSON, NULL, 0);
     }
     char *sub_uri = uri + strlen("/echo/");
     size_t sub_uri_len = strlen(sub_uri);
-    return coap_reply_simple(pkt, COAP_CODE_CONTENT, buf, len, COAP_FORMAT_TEXT,
+    return coap_reply_simple(pkt, COAP_CODE_CONTENT, buf, len, COAP_FORMAT_JSON,
                              (uint8_t *)sub_uri, sub_uri_len);
 }
 
@@ -37,7 +37,7 @@ wot_td_uri_t wot_td_echo_aff_form_href = {0};
 wot_td_extension_t wot_td_echo_form_coap = {0};
 
 wot_td_content_type_t wot_td_echo_content_type = {
-        .media_type = CONTENT_TYPE_TEXT_PLAIN,
+        .media_type = CONTENT_TYPE_JSON,
         .media_type_paramter = NULL
 };
 
@@ -52,11 +52,39 @@ wot_td_form_t wot_td_echo_aff_form = {
 wot_td_int_affordance_t wot_echo_int_affordance = {
         .forms = &wot_td_echo_aff_form
 };
+
+wot_td_data_schema_t wot_hello_prop_data = {
+        .read_only = true,
+        .write_only = false,
+        .json_type = JSON_TYPE_STRING
+};
+
+wot_td_data_schema_map_t wot_hello_prop_map = {
+        .key = "hello",
+        .value = &wot_hello_prop_data
+};
+
+wot_td_object_required_t wot_echo_required = {
+        .value = "hello"
+};
+
+wot_td_object_schema_t wot_echo_data_schema_obj = {
+        .properties = &wot_hello_prop_map,
+        .required = &wot_echo_required
+};
+
+wot_td_data_schema_t wot_echo_data_schema = {
+        .json_type = JSON_TYPE_OBJECT,
+        .read_only = true,
+        .write_only = false,
+        .schema = &wot_echo_data_schema_obj
+};
+
 wot_td_prop_affordance_t wot_echo_affordance = {
         .observable = false,
         .key = "echo",
         .int_affordance = &wot_echo_int_affordance,
-        .data_schema = NULL,
+        .data_schema = &wot_echo_data_schema,
         .next = NULL,
 };
 
