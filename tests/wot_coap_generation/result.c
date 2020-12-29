58556b435040bc3ab3ce85456a73db3792299bb9
Jan Romann <jan.romann@gmail.com>
2020-12-29 03:55:37 +0100

    Add brightness example to affordances json file
    Unrealistic GET for toggle affordance has been removed at the same time

--- tests/wot_coap_generation/result.c
+++ tests/wot_coap_generation/result.c
@@ -9,37 +9,41 @@
 #include "net/wot/coap.h"
 #include "net/wot/coap/config.h"
 
-extern ssize_t _led_toggle_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx);
+extern ssize_t _led_brightness_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx);
+extern ssize_t _led_toggle_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx);
 
-static ssize_t wot_led_toggle_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx)
+static ssize_t wot_led_brightness_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx)
+{
+    return _led_brightness_handler(&pdu, &buf, len, &ctx);
+}
+
+static ssize_t wot_led_toggle_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx)
 {
     return _led_toggle_handler(&pdu, &buf, len, &ctx);
 }
 
 const coap_resource_t _wot_coap_resources[] = {
-    {"/led/toggle", COAP_POST | COAP_GET, wot_led_toggle_handler, NULL},
+    {"/led/brightness", COAP_GET | COAP_POST, wot_led_brightness_handler, NULL},
+    {"/led/toggle", COAP_POST, wot_led_toggle_handler, NULL},
 };
 
 static const char *_wot_link_params[] = {
-    NULL
-}
-
-static gcoap_listener_t _wot_coap_listener = {
-    &_wot_coap_resources[0],
-    ARRAY_SIZE(_wot_coap_resources),
-    _wot_encode_link,
     NULL,
-    NULL
-};
+    NULL}
+
+static gcoap_listener_t _wot_coap_listener = {&_wot_coap_resources[0], ARRAY_SIZE(_wot_coap_resources), _wot_encode_link, NULL, NULL};
 
 static ssize_t _wot_encode_link(const coap_resource_t *resource, char *buf,
-                            size_t maxlen, coap_link_encoder_ctx_t *context) {
+                                size_t maxlen, coap_link_encoder_ctx_t *context)
+{
     ssize_t res = gcoap_encode_link(resource, buf, maxlen, context);
-    if (res > 0) {
-        if (_wot_link_params[context->link_pos]
-                && (strlen(_wot_link_params[context->link_pos]) < (maxlen - res))) {
-            if (buf) {
-                memcpy(buf+res, _wot_link_params[context->link_pos],
+    if (res > 0)
+    {
+        if (_wot_link_params[context->link_pos] && (strlen(_wot_link_params[context->link_pos]) < (maxlen - res)))
+        {
+            if (buf)
+            {
+                memcpy(buf + res, _wot_link_params[context->link_pos],
                        strlen(_wot_link_params[context->link_pos]));
             }
             return res + strlen(_wot_link_params[context->link_pos]);
