ab99eb6607129d3f1febd455614913a55ab915d0
Jan Romann <jan.romann@gmail.com>
2020-12-30 02:37:26 +0100

    Add led controller code to test the implementation

--- tests/wot_coap_generation/main.c
+++ tests/wot_coap_generation/main.c
@@ -6,19 +6,31 @@
 #include "net/wot/coap.h"
 #include "shell.h"
 
+#ifndef IS_NATIVE
+extern int led_cmd(int argc, char **argv);
+extern void led_cmd_init(void);
+#endif
+
 #define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 static const shell_command_t shell_commands[] = {
-        { NULL, NULL, NULL }
-};
+#ifndef IS_NATIVE
+    {"led", "Control an LED.", led_cmd},
+#endif
+    {NULL, NULL, NULL}};
 
-int main(void) {
+int main(void)
+{
     xtimer_sleep(3);
     /* for the thread running the shell */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     //Todo: Implement auto init
     wot_td_coap_server_init();
+
+#ifndef IS_NATIVE
+    led_cmd_init();
+#endif
 
     /* start shell */
     puts("All up, running the shell now");
