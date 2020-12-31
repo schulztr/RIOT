139d11d6507d2d7b799cac7753540875cace2227
Philipp-Alexander Blum <philipp-blum@jakiku.de>
2020-12-31 14:12:20 +0100

    Use form instead of affordance for endpoint definition

--- sys/include/net/wot/coap.h
+++ sys/include/net/wot/coap.h
@@ -6,23 +6,26 @@
 
 typedef struct {
     const coap_resource_t *coap_resource;
+    wot_td_form_t *form;
     wot_td_prop_affordance_t *affordance;
 } wot_td_coap_prop_affordance_t;
 
 typedef struct {
     const coap_resource_t *coap_resource;
+    wot_td_form_t *form;
     wot_td_action_affordance_t *affordance;
 } wot_td_coap_action_affordance_t;
 
 typedef struct {
     const coap_resource_t *coap_resource;
+    wot_td_form_t *form;
     wot_td_event_affordance_t *affordance;
 } wot_td_coap_event_affordance_t;
 
 void wot_td_coap_server_init(void);
 
-int wot_td_coap_prop_add(wot_td_thing_t *thing, wot_td_coap_prop_affordance_t *property);
-int wot_td_coap_action_add(wot_td_thing_t *thing, wot_td_coap_action_affordance_t *action);
-int wot_td_coap_event_add(wot_td_thing_t *thing, wot_td_coap_event_affordance_t *event);
+int wot_td_coap_prop_add(wot_td_thing_t *thing, wot_td_coap_prop_affordance_t *coap_aff);
+int wot_td_coap_action_add(wot_td_thing_t *thing, wot_td_coap_action_affordance_t *coap_aff);
+int wot_td_coap_event_add(wot_td_thing_t *thing, wot_td_coap_event_affordance_t *coap_aff);
 
 #endif //WOT_COAP_H
