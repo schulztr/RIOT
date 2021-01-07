b09d04f223b9579d2ee00e013a84ac8fbcf41c58
Jan Romann <jan.romann@gmail.com>
2021-01-07 02:57:21 +0100

    Update example output

--- tests/wot_coap_generation/wot_coap_config.c
+++ tests/wot_coap_generation/wot_coap_config.c
@@ -84,47 +84,119 @@ wot_td_form_t wot_td_echo_aff_form_0 = {
     .next = NULL,
 };
 
-wot_td_int_affordance_t wot_echo_int_affordance = {
+wot_td_type_t wot_td_echo_uri_variable_d_data_map_data_schema_type_0 = {
+    .value = "eg:Direction",
+    .next = NULL,
+};
+
+wot_td_data_schema_t wot_td_echo_uri_variable_d_data_map_data_schema = {
+    .type = &wot_td_echo_uri_variable_d_data_map_data_schema_type_0,
+    .json_type = JSON_TYPE_INTEGER,
+};
+
+wot_td_data_schema_map_t wot_td_echo_uri_variable_d_data_map = {
+    .key = "d",
+    .value = &wot_td_echo_uri_variable_d_data_map_data_schema,
+    .next = NULL,
+};
+
+wot_td_type_t wot_td_echo_uri_variable_p_data_map_data_schema_type_0 = {
+    .value = "eg:SomeKindOfAngle",
+    .next = NULL,
+};
+
+wot_td_data_schema_t wot_td_echo_uri_variable_p_data_map_data_schema = {
+    .type = &wot_td_echo_uri_variable_p_data_map_data_schema_type_0,
+    .json_type = JSON_TYPE_INTEGER,
+};
+
+wot_td_data_schema_map_t wot_td_echo_uri_variable_p_data_map = {
+    .key = "p",
+    .value = &wot_td_echo_uri_variable_p_data_map_data_schema,
+    .next = &wot_td_echo_uri_variable_d_data_map,
+};
+
+wot_td_multi_lang_t wot_td_echo_int_affordance_title_1 = {
+    .tag = "en",
+    .value = "English Title",
+    .next = NULL,
+};
+
+wot_td_multi_lang_t wot_td_echo_int_affordance_title_0 = {
+    .tag = "de",
+    .value = "Deutscher Titel",
+    .next = &wot_td_echo_int_affordance_title_1,
+};
+
+wot_td_multi_lang_t wot_td_echo_int_affordance_description_1 = {
+    .tag = "en",
+    .value = "English description",
+    .next = NULL,
+};
+
+wot_td_multi_lang_t wot_td_echo_int_affordance_description_0 = {
+    .tag = "de",
+    .value = "Deutsche Beschreibung",
+    .next = &wot_td_echo_int_affordance_description_1,
+};
+
+wot_td_type_t wot_td_echo_int_affordance_type_0 = {
+    .value = "Type",
+    .next = NULL,
+};
+
+wot_td_int_affordance_t wot_td_echo_int_affordance = {
+    .type = &wot_td_echo_int_affordance_type_0,
+    .descriptions = &wot_td_echo_int_affordance_description_0,
+    .titles = &wot_td_echo_int_affordance_title_0,
+    .uri_variables = &wot_td_echo_uri_variable_p_data_map,
     .forms = &wot_td_echo_aff_form_0,
 };
 
-wot_td_object_required_t wot_echo_hello_required = {
+wot_td_object_required_t wot_td_echo_affordance_data_schema_hello_required = {
     .value = "hello",
 };
 
-wot_td_data_schema_t wot_echo_hello_data_schema = {
+wot_td_data_schema_t wot_td_echo_affordance_data_schema_hello_data_map_data_schema = {
     .json_type = JSON_TYPE_STRING,
     .read_only = true,
     .write_only = false,
 };
 
-wot_td_data_schema_map_t wot_echo_hello_data_map = {
+wot_td_data_schema_map_t wot_td_echo_affordance_data_schema_hello_data_map = {
     .key = "hello",
-    .value = &wot_echo_hello_data_schema,
+    .value = &wot_td_echo_affordance_data_schema_hello_data_map_data_schema,
+    .next = NULL,
 };
 
-wot_td_object_schema_t wot_echo_data_schema_obj = {
-    .properties = &wot_echo_hello_data_map,
-    .required = &wot_echo_hello_required,
+wot_td_object_schema_t wot_td_echo_affordance_data_schema_object = {
+    .properties = &wot_td_echo_affordance_data_schema_hello_data_map,
+    .required = &wot_td_echo_affordance_data_schema_hello_required,
 };
 
-wot_td_data_schema_t wot_echo_data_schema = {
+wot_td_type_t wot_td_echo_affordance_data_schema_type_0 = {
+    .value = "Type",
+    .next = NULL,
+};
+
+wot_td_data_schema_t wot_td_echo_affordance_data_schema = {
+    .type = &wot_td_echo_affordance_data_schema_type_0,
     .json_type = JSON_TYPE_OBJECT,
     .read_only = true,
     .write_only = false,
-    .schema = &wot_echo_data_schema_obj,
+    .schema = &wot_td_echo_affordance_data_schema_object,
 };
 
-wot_td_prop_affordance_t wot_echo_affordance = {
+wot_td_prop_affordance_t wot_td_echo_affordance = {
     .key = "echo",
-    .int_affordance = &wot_echo_int_affordance,
-    .data_schema = &wot_echo_data_schema,
+    .int_affordance = &wot_td_echo_int_affordance,
+    .data_schema = &wot_td_echo_affordance_data_schema,
     .next = NULL,
 };
 
 wot_td_coap_prop_affordance_t wot_coap_echo_affordance = {
     .coap_resource = &_wot_coap_resources[0],
-    .affordance = &wot_echo_affordance,
+    .affordance = &wot_td_echo_affordance,
     .form = &wot_td_echo_aff_form_0,
 };
 
@@ -144,19 +216,19 @@ wot_td_form_t wot_td_status_aff_form_0 = {
     .next = NULL,
 };
 
-wot_td_int_affordance_t wot_status_int_affordance = {
+wot_td_int_affordance_t wot_td_status_int_affordance = {
     .forms = &wot_td_status_aff_form_0,
 };
 
-wot_td_prop_affordance_t wot_status_affordance = {
+wot_td_prop_affordance_t wot_td_status_affordance = {
     .key = "status",
-    .int_affordance = &wot_status_int_affordance,
+    .int_affordance = &wot_td_status_int_affordance,
     .next = NULL,
 };
 
 wot_td_coap_prop_affordance_t wot_coap_status_affordance = {
     .coap_resource = &_wot_coap_resources[1],
-    .affordance = &wot_status_affordance,
+    .affordance = &wot_td_status_affordance,
     .form = &wot_td_status_aff_form_0,
 };
 
@@ -176,19 +248,45 @@ wot_td_form_t wot_td_toggle_aff_form_0 = {
     .next = NULL,
 };
 
-wot_td_int_affordance_t wot_toggle_int_affordance = {
+wot_td_multi_lang_t wot_td_toggle_int_affordance_title_1 = {
+    .tag = "en",
+    .value = "English title",
+    .next = NULL,
+};
+
+wot_td_multi_lang_t wot_td_toggle_int_affordance_title_0 = {
+    .tag = "de",
+    .value = "Deutscher Titel",
+    .next = &wot_td_toggle_int_affordance_title_1,
+};
+
+wot_td_multi_lang_t wot_td_toggle_int_affordance_description_1 = {
+    .tag = "en",
+    .value = "English description",
+    .next = NULL,
+};
+
+wot_td_multi_lang_t wot_td_toggle_int_affordance_description_0 = {
+    .tag = "de",
+    .value = "Deutsche Beschreibung",
+    .next = &wot_td_toggle_int_affordance_description_1,
+};
+
+wot_td_int_affordance_t wot_td_toggle_int_affordance = {
+    .descriptions = &wot_td_toggle_int_affordance_description_0,
+    .titles = &wot_td_toggle_int_affordance_title_0,
     .forms = &wot_td_toggle_aff_form_0,
 };
 
-wot_td_action_affordance_t wot_toggle_affordance = {
+wot_td_action_affordance_t wot_td_toggle_affordance = {
     .key = "toggle",
-    .int_affordance = &wot_toggle_int_affordance,
+    .int_affordance = &wot_td_toggle_int_affordance,
     .next = NULL,
 };
 
 wot_td_coap_action_affordance_t wot_coap_toggle_affordance = {
     .coap_resource = &_wot_coap_resources[2],
-    .affordance = &wot_toggle_affordance,
+    .affordance = &wot_td_toggle_affordance,
     .form = &wot_td_toggle_aff_form_0,
 };
 
