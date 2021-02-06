9d8001c824584ca52c9046718dd7240116db393c
Philipp-Alexander Blum <philipp-blum@jakiku.de>
2021-02-06 10:06:42 +0100

    Refactoring wot_forms in coap

--- sys/include/net/wot/coap.h
+++ sys/include/net/wot/coap.h
@@ -4,28 +4,13 @@
 #include "net/wot.h"
 #include "net/gcoap.h"
 
-typedef struct {
+typedef struct wot_td_coap_form {
     const coap_resource_t *coap_resource;
     wot_td_form_t *form;
-    wot_td_prop_affordance_t *affordance;
-} wot_td_coap_prop_affordance_t;
+    struct wot_td_coap_form *next;
+} wot_td_coap_form_t;
 
-typedef struct {
-    const coap_resource_t *coap_resource;
-    wot_td_form_t *form;
-    wot_td_action_affordance_t *affordance;
-} wot_td_coap_action_affordance_t;
-
-typedef struct {
-    const coap_resource_t *coap_resource;
-    wot_td_form_t *form;
-    wot_td_event_affordance_t *affordance;
-} wot_td_coap_event_affordance_t;
-
+void wot_td_coap_add_forms(wot_td_coap_form_t *coap_forms);
 void wot_td_coap_server_init(void);
-
-int wot_td_coap_prop_add(wot_td_thing_t *thing, wot_td_coap_prop_affordance_t *coap_aff);
-int wot_td_coap_action_add(wot_td_thing_t *thing, wot_td_coap_action_affordance_t *coap_aff);
-int wot_td_coap_event_add(wot_td_thing_t *thing, wot_td_coap_event_affordance_t *coap_aff);
 
 #endif //WOT_COAP_H
