1d6147c6700f12bb36abe0de94d21dc45d20e732
Jan Romann <jan.romann@uni-bremen.de>
2021-04-18 14:44:40 +0200

    Add copyright and license information

--- dist/tools/wot/webofthings.py
+++ dist/tools/wot/webofthings.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2021 Universität Bremen
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
 import os
 import json
 import copy
